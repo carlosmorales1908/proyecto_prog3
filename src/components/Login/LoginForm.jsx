59ff65e83cc92b26b546e47335da0aee0163f397
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2024-07-18 22:06:35 -0300

    update LoginForm

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,67 +1,72 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../../context/auth.contex";
-import { Navigate,useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { PrivateRoutes } from "../../routes/routes";
+import useForm from "../../hooks/useForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const { login,isAutheticated  } = useContext(AuthContext);
+  const { login, isAutheticated } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await login({ username, password });
-      navigate(PrivateRoutes.HOME, { replace: true });
-    } catch (error) {
-      console.error("Login error:", error);
+  const initialState = { username: "", password: "" };
+  const { values, handleChange, handleSubmit, getRef } = useForm(
+    initialState,
+    async () => {
+      try {
+        await login({ username: values.username, password: values.password });
+        navigate(PrivateRoutes.HOME, { replace: true });
+      } catch (error) {
+        console.error("Login error:", error);
+      }
     }
-  };
+  );
 
   return (
     <>
-    {isAutheticated ? (
-      <Navigate replace to={PrivateRoutes.HOME} />
-    ) : (
-       <form>
+      {isAutheticated ? (
+        <Navigate replace to={PrivateRoutes.HOME} />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="inputUsername" className="form-label text-light ">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control bg-transparent text-light border border-success "
+              id="inputUsername"
+              name="username"
+              placeholder="Username"
+              value={values.username}
+              onChange={handleChange}
+              ref={getRef("username")}
+              required
+            />
+          </div>
 
-      <div className="mb-3">
-        <label htmlFor="inputEmail" className="form-label">
-          Email
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="inputEmail"
-          placeholder="email address"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
+          <div className="mb-3">
+            <label htmlFor="inputPassword" className="form-label text-light">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control bg-transparent text-light border border-success"
+              id="inputPassword"
+              name="password"
+              placeholder="********"
+              value={values.password}
+              onChange={handleChange}
+              ref={getRef("password")}
+              required
+            />
+          </div>
 
-      <div className="mb-3">
-        <label htmlFor="inputPassword" className="form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="inputPassword"
-          placeholder="********"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-
-      <button type="button" className="btn btn-outline-primary d-block mx-auto" onClick={handleSubmit}>
-        Login
-      </button>
-    </form>
-    )}
-   </>
+          <button type="submit" className="btn btn-outline-success w-100">
+            Login
+          </button>
+        </form>
+      )}
+    </>
   );
 };
 
