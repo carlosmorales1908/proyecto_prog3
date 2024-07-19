651a6d694bca96a7f956cedf5e27096c29cbd90e
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2024-07-18 22:07:24 -0300

    update Login.page

--- src/pages/Login.page.jsx
+++ src/pages/Login.page.jsx
@@ -1,24 +1,24 @@
-
 import React from "react";
 import LoginForm from "../components/Login/LoginForm";
 
 const LoginPage = () => {
   return (
-    <div className="login-page d-flex align-items-center justify-content-center vh-100">
-      <div className="container row justify-content-center">
+    <div className="login-page d-flex align-items-center justify-content-center vh-100 bg-black bg-gradient text-light">
+      <div className="container">
         <div className="row justify-content-center">
-          <div className="col-md-6">
-            <div className="card card-custom mt-5 bg-dark text-light">
-              <div className="card-body">
-                <h2 className="card-title mb-4 text-center">
-                  <strong>Log in to MusicApp</strong>
-                </h2>
-                <LoginForm />
-                <div className="mt-3 text-center">
-                  <p className="text">
-                    <strong>Don't have an account?</strong>
-                  </p>
+          <div className="col-md-6 col-lg-4">
+            <div className="card card-custom bg-black p-2 text-white bg-opacity-75 text-light shadow-lg">
+              <div className="card-body p-4">
+                <div className="text-center mb-4">
+                  <i
+                    className="bi bi-music-note-beamed"
+                    style={{ fontSize: "2rem" }}
+                  ></i>
+                  <h2 className="card-title mt-3">
+                    <strong>Log in to MusicApp</strong>
+                  </h2>
                 </div>
+                <LoginForm />
               </div>
             </div>
           </div>
