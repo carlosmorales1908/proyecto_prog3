4be0dad8a4f32425a23ced6339ec7f8af999b544
Juan Ceballo <edu17290@gmail.com>
2024-07-25 09:41:12 -0300

    added style and generic image to playlist component

--- src/components/PlaylistCard/PlaylistCard.jsx
+++ src/components/PlaylistCard/PlaylistCard.jsx
@@ -1,12 +1,14 @@
+import "../PlaylistCard/PlaylistStyle.css";
+
 export const PlaylistCard = ({ playlist }) => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <div className="card-body text-center">
-        <h5 className="card-title">{playlist.name}</h5>
-        <p className="card-text">{playlist.description}</p>
-        <a href="#" className="card-link">
-          Ir a la playlist
-        </a>
+    <div className="card" style={{ width: "300px" }}>
+      <div className="container mt-2">
+        <img src="src\assets\micro.jpeg" className="card-img-top rounded-4" />
+        <div className="card-body text-center">
+          <h5 className="card-title fs-2">{playlist.name}</h5>
+          <p className="card-text fs-5">{playlist.description}</p>
+        </div>
       </div>
     </div>
   );
