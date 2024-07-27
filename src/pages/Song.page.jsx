e690ed31428e96d491180345e1e71fb3d593cd67
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2024-07-27 16:56:04 -0300

    Update Song.page

--- src/pages/Song.page.jsx
+++ src/pages/Song.page.jsx
@@ -3,11 +3,8 @@ import SongList from "../components/Song/SongList";
 
 const SongsPage = () => {
   return (
-    <div>
-      <div className="container mt-4">
-        <h1 className="mb-4">Lista de Canciones</h1>
-        <SongList />
-      </div>
+    <div className="d-flex flex-column">
+          <SongList />
     </div>
   );
 };
