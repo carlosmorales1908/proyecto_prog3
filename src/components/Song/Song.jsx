0c23bd5689bd31a2d1dfe0078814ab2d5429dadb
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2024-07-27 17:23:58 -0300

    Update Song

--- src/components/Song/Song.jsx
+++ src/components/Song/Song.jsx
@@ -1,11 +1,10 @@
 import React, { useRef, useState, useEffect } from "react";
 import SongMenu from "./SongMenu";
-
+import "./Song.css";
 const Song = ({ song, onPlay, isPlaying, index }) => {
   const [isHovered, setIsHovered] = useState(false);
-  const [showMenu, setShowMenu] = useState(false); 
+  const [showMenu, setShowMenu] = useState(false);
   const audioRef = useRef(null);
-
 
   useEffect(() => {
     if (isPlaying) {
@@ -30,15 +29,18 @@ const Song = ({ song, onPlay, isPlaying, index }) => {
 
   return (
     <div
-      className={`position-relative d-flex align-items-center p-2 rounded-3 ${isHovered ? 'bg-secondary' : 'bg-transparent'} text-light`}
-      
+      className={`position-relative d-flex align-items-center p-2 rounded-3 ${
+        isHovered ? "bg-secondary" : "bg-transparent"
+      } text-light`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="col-1">
         {isHovered || isPlaying ? (
           <i
-            className={`bi ${isPlaying ? "bi-pause-fill" : "bi-play-fill"} fs-5`}
+            className={`bi ${
+              isPlaying ? "bi-pause-fill" : "bi-play-fill"
+            } fs-5`}
             role="button"
             onClick={onPlay}
           ></i>
@@ -46,10 +48,14 @@ const Song = ({ song, onPlay, isPlaying, index }) => {
           index
         )}
       </div>
-      <div className="flex-grow-1 me-3"  onClick={onPlay} role="button">
-        <h6 className="mb-1">{song.title}</h6>
+      <div className="flex-grow-1 me-3" onClick={onPlay} role="button">
+        <h6 className={`mb-1 ${!song.song_file && "text-danger"}`}>{song.title}</h6>
       </div>
-      <div className="text-light position-relative flex-grow-2" role="button" style={{ right: "36%" }}>
+      <div
+        className="text-light position-relative flex-grow-2"
+        role="button"
+        style={{ right: "36%" }}
+      >
         <small>{song.duration}</small>
       </div>
       <div className="d-flex align-items-center ms-3 position-relative">
@@ -59,15 +65,15 @@ const Song = ({ song, onPlay, isPlaying, index }) => {
           onEnded={() => onPlay(null)}
         />
         <i
-      
-          className={`bi bi-three-dots fs-5 ${isHovered ? 'd-block' : 'd-none'}`}
+          className={`bi bi-three-dots fs-5 ${
+            isHovered ? "d-block" : "d-none"
+          }`}
           onClick={handleMenuClick}
         ></i>
         {showMenu && (
           <SongMenu
             onClose={() => setShowMenu(false)}
             onAction={handleMenuAction}
-         
             className={""}
           />
         )}
