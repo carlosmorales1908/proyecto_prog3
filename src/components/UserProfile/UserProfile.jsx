051c10d9b3adafc9a49f6a204b53ecafbb31f0e7
Juan Ceballo <edu17290@gmail.com>
2024-07-30 14:38:43 -0300

    added option to change user profile picture

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "../../context/auth.contex";
 import UserProfileService from "../../services/profile.services";
 import parseEmail from "../../utility/parseEmail";
@@ -9,30 +9,58 @@ const UserProfile = () => {
   const [userProfile, setUserProfile] = useState([]);
   const [email, setEmail] = useState("");
   const [showEmail, setShowEmail] = useState(false);
-
-  const hanleShowEmail = () => {
-    setShowEmail(!showEmail);
-  };
+  const [profileImg, setProfileImg] = useState(null);
+  const fileInputRef = useRef(null);
 
   useEffect(() => {
     const userProfileService = new UserProfileService(token);
     userProfileService.getProfile().then((profile) => {
       setUserProfile(profile);
       setEmail(profile.email || "");
+      setProfileImg(profile.image || "src/assets/sin_perfil.jpeg");
     });
   }, [token]);
+
+  const hanleShowEmail = () => {
+    setShowEmail(!showEmail);
+  };
+
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleChangeImg = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setProfileImg(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <>
       <h1 className="mb-3">USER PROFILE</h1>
       <div className="container card-profile g-0">
         <div className="user-info">
           <div className="header-card d-flex">
-            <img
-              src="src\assets\sin_perfil.jpeg"
-              className="rounded-circle m-2"
-              style={{ width: "150px", height: "150px" }}
-            />
-            <div className="user-name-header d-block justify-content-center mx-5 fs-2 mt-3 fw-bold">
+            <div>
+              <img
+                src={profileImg}
+                className="header-card-img rounded-circle m-2"
+                onClick={handleImageClick}
+              />
+              <input
+                ref={fileInputRef}
+                type="file"
+                accept="image/*"
+                className="d-none"
+                onChange={handleChangeImg}
+              />
+            </div>
+            <div className="user-name-header d-block mx-5 fs-2 mt-5 fw-bold">
               <p>{userProfile.first_name}</p>
             </div>
           </div>
