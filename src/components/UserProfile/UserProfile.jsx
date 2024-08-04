963599beeb4a76e3376c52f0cc1020b0e523bcb2
Juan Ceballo <edu17290@gmail.com>
2024-08-03 21:03:12 -0300

    now can edit fields on UserProfile

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -3,25 +3,41 @@ import { AuthContext } from "../../context/auth.contex";
 import UserProfileService from "../../services/profile.services";
 import parseEmail from "../../utility/parseEmail";
 import "./UserProfile.css";
+import Spinner from "../Spinner/Spinner";
 
 const UserProfile = () => {
   const { token } = useContext(AuthContext);
-  const [userProfile, setUserProfile] = useState([]);
+  const [userProfile, setUserProfile] = useState(null);
   const [email, setEmail] = useState("");
   const [showEmail, setShowEmail] = useState(false);
   const [profileImg, setProfileImg] = useState(null);
+  const [imgFile, setImgFile] = useState(null);
   const fileInputRef = useRef(null);
+
+  const [editMode, setEditMode] = useState(false);
+  const [data, setData] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
 
   useEffect(() => {
     const userProfileService = new UserProfileService(token);
     userProfileService.getProfile().then((profile) => {
       setUserProfile(profile);
       setEmail(profile.email || "");
-      setProfileImg(profile.image || "src/assets/sin_perfil.jpeg");
+      setProfileImg(profile.image || null);
+      setData({
+        username: profile.username || "",
+        firstName: profile.first_name || "",
+        lastName: profile.last_name || "",
+        email: profile.email || "",
+      });
     });
   }, [token]);
 
-  const hanleShowEmail = () => {
+  const handleShowEmail = () => {
     setShowEmail(!showEmail);
   };
 
@@ -32,6 +48,7 @@ const UserProfile = () => {
   const handleChangeImg = (event) => {
     const file = event.target.files[0];
     if (file) {
+      setImgFile(file);
       const reader = new FileReader();
       reader.onload = (e) => {
         setProfileImg(e.target.result);
@@ -39,6 +56,79 @@ const UserProfile = () => {
       reader.readAsDataURL(file);
     }
   };
+
+  const handleEdit = () => {
+    setEditMode(true);
+  };
+
+  const handleCancelEdit = () => {
+    setEditMode(false);
+    if (userProfile) {
+      setData({
+        username: userProfile.username,
+        firstName: userProfile.first_name,
+        lastName: userProfile.last_name,
+        email: userProfile.email,
+      });
+    }
+  };
+
+  const handleSaveChanges = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("first_name", data.firstName);
+    formData.append("last_name", data.lastName);
+    formData.append("email", data.email);
+    imgFile && formData.append("image", imgFile);
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_URL}users/profiles/${
+          userProfile.user__id
+        }/`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+          body: formData,
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error al actualizar los datos");
+      }
+
+      const updatedData = await response.json();
+
+      setData((prevData) => ({
+        ...prevData,
+        firstName: updatedData.first_name,
+      }));
+
+      setEditMode(false);
+    } catch (error) {
+      console.error(error);
+    }
+    fetch(
+      `${import.meta.env.VITE_BASE_URL}users/profiles/${userProfile.user__id}/`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+        body: formData,
+      }
+    );
+    setEditMode(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  if (!userProfile) return <Spinner />;
 
   return (
     <>
@@ -48,17 +138,24 @@ const UserProfile = () => {
           <div className="header-card d-flex">
             <div>
               <img
-                src={profileImg}
+                src={
+                  profileImg
+                    ? `${import.meta.env.VITE_BASE_URL}${userProfile.image}`
+                    : "src/assets/sin_perfil.jpeg"
+                }
                 className="header-card-img rounded-circle m-2"
                 onClick={handleImageClick}
+                alt="Profile"
               />
-              <input
-                ref={fileInputRef}
-                type="file"
-                accept="image/*"
-                className="d-none"
-                onChange={handleChangeImg}
-              />
+              {editMode && (
+                <input
+                  ref={fileInputRef}
+                  type="file"
+                  accept="image/*"
+                  className="d-none"
+                  onChange={handleChangeImg}
+                />
+              )}
             </div>
             <div className="user-name-header d-block mx-5 fs-2 mt-5 fw-bold">
               <p>{userProfile.first_name}</p>
@@ -66,25 +163,93 @@ const UserProfile = () => {
           </div>
           <div className="body-card m-2">
             <div className="user-name-body fs-5">
-              <p className="my-0">Nombre de Usuario:</p>
-              <p>{userProfile.username}</p>
+              <p className="my-1 fw-bold fs-4">Nombre de Usuario:</p>
+              {editMode ? (
+                <input
+                  type="text"
+                  name="username"
+                  value={data.username}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                <p>{userProfile.username}</p>
+              )}
             </div>
-            <div className="full-name-body fs-5">
-              <p className="my-0">Nombre Completo:</p>
-              {userProfile.first_name} {userProfile.last_name}
+            <div className="first-name-body fs-5">
+              <p className="my-1 fw-bold fs-4">Nombres:</p>
+              {editMode ? (
+                <input
+                  type="text"
+                  name="firstName"
+                  value={data.firstName}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                <p>{userProfile.first_name}</p>
+              )}
             </div>
-            <div className="email-body fs-5 mt-3">
-              <p>Correo Electrónico:</p>
-              <div className="emial-info d-flex">
-                <p>{!showEmail ? parseEmail(email) : email}</p>
-                <p
-                  onClick={hanleShowEmail}
-                  className="text-primary mx-2"
-                  style={{ cursor: "pointer" }}
+            <div className="last-name-body fs-5">
+              <p className="my-1 fw-bold fs-4">Apellidos:</p>
+              {editMode ? (
+                <input
+                  type="text"
+                  name="lastName"
+                  value={data.lastName}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                <p>{userProfile.last_name}</p>
+              )}
+            </div>
+            <div className="email-body fs-5 mt-3 mb-3">
+              <p className="fw-bold fs-4">Correo Electrónico:</p>
+              {editMode ? (
+                <input
+                  type="email"
+                  name="email"
+                  value={data.email}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                <div className="email-info d-flex">
+                  <p>{!showEmail ? parseEmail(email) : email}</p>
+                  <p
+                    onClick={handleShowEmail}
+                    className="text-primary mx-2"
+                    style={{ cursor: "pointer" }}
+                  >
+                    Mostrar
+                  </p>
+                </div>
+              )}
+            </div>
+            <div className="buttons-container d-flex justify-content-center">
+              {editMode ? (
+                <>
+                  <button
+                    type="submit"
+                    className="btn btn-success mx-2 fs-5"
+                    onClick={handleSaveChanges}
+                  >
+                    Guardar
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary mx-2 fs-5"
+                    onClick={handleCancelEdit}
+                  >
+                    Cancelar
+                  </button>
+                </>
+              ) : (
+                <button
+                  type="button"
+                  className="btn btn-primary fs-5"
+                  onClick={handleEdit}
                 >
-                  Mostrar
-                </p>
-              </div>
+                  Editar Perfil de Usuario
+                </button>
+              )}
             </div>
           </div>
         </div>
@@ -92,4 +257,14 @@ const UserProfile = () => {
     </>
   );
 };
+
 export default UserProfile;
+
+/*
+user__id
+username
+first_name
+last_name
+email
+image
+*/
