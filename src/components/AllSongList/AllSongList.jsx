b037af17a70276245f22d960a5c74a7cb6f30133
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2024-08-08 14:37:35 -0300

    Update AllSongList

--- src/components/AllSongList/AllSongList.jsx
+++ src/components/AllSongList/AllSongList.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useCallback, useEffect, useRef } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import SongList from "../Song/SongList";
 import Spinner from "../Spinner/Spinner";
 import Search from "../Search/Search";
@@ -8,52 +8,39 @@ import useInfiniteScroll from "../../hooks/useInfiniteScroll";
 
 const AllSongList = () => {
   const [page, setPage] = useState(1);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
   const [songs, setSongs] = useState([]);
   const [nextUrl, setNextUrl] = useState(null);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
   const { token } = useContext(AuthContext);
-
   const songServiceRef = useRef(new SongService(token));
 
-  const fetchSongs = useCallback(async () => {
-    if (!nextUrl && page !== 1) return;
-
-    setIsLoading(true);
-    setIsError(false);
-    try {
-      const pageSize = 10;
-      const data = await songServiceRef.current.getSongsByPage(page, pageSize);
-      setSongs((prevSongs) => [...prevSongs, ...data.results]);
-      setNextUrl(data.next || null);
-      if (data.next) {
-        setPage((prevPage) => prevPage + 1);
-      }
-    } catch (error) {
-      if (error.response && error.response.status === 404) {
-        setNextUrl(null);
-      } else {
-        setIsError(true);
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  }, [page, nextUrl]);
-
   useEffect(() => {
-    fetchSongs();
-  }, [fetchSongs]);
-
-  const filteredSongs = songs.filter((song) =>
-    song.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+    const doFetch = async () => {
+      setIsLoading(true);
+      try {
+        const data = await songServiceRef.current.getSongsByPage(page, 10);
+        setSongs((prevSongs) => [...prevSongs, ...data.results]);
+        setNextUrl(data.next);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    doFetch();
+  }, [page, token]);
 
   const loadMoreSongs = () => {
     if (nextUrl) setPage((prevPage) => prevPage + 1);
   };
 
   const lastElementRef = useInfiniteScroll(nextUrl, isLoading, loadMoreSongs);
+
+  const filteredSongs = songs.filter((song) =>
+    song.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   const handleSearch = (query) => {
     setSearchQuery(query);
