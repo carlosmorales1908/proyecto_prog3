340d2fe52cd56d59a6652b7a61e9514d7aaba116
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2024-08-10 14:58:33 -0300

    Added 'layout' prop to Spinner component for conditional minHeight styling

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,14 +1,11 @@
-import React from "react";
-
-const Spinner = () => {
-    return(
-        <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "500px"
-        }}>
-        <div className="spinner-border color-primary" role="status">
-          <span className="visually-hidden"></span>
-        </div>
+const Spinner = ({ layout = null }) => {
+  return (
+      <div className="d-flex justify-content-center align-items-center" style={{ minHeight: layout ? "500px" : "auto" }}>
+          <div className="spinner-border color-primary" role="status">
+              <span className="visually-hidden"></span>
+          </div>
       </div>
-    )
+  );
 };
 
 export default Spinner;
