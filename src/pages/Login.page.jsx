9cc68e49e3710f1d6e56ae5cef442ee269050714
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2024-08-10 14:59:05 -0300

    Added LoginCard in Login.page

--- src/pages/Login.page.jsx
+++ src/pages/Login.page.jsx
@@ -1,33 +1,9 @@
-import React from "react";
-import LoginForm from "../components/Login/LoginForm";
+import LoginCard from "../components/Login/LoginCard";
 
 const LoginPage = () => {
-  return (
-    <div className="login-page d-flex align-items-center justify-content-center vh-100 bg-black bg-gradient text-light">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <div className="card card-custom bg-black p-2 text-white bg-opacity-75 text-light shadow-lg">
-              <div className="card-body p-4">
-                <div className="text-center mb-4">
-                  <img
-                    src="/minilogo3.png"
-                    alt="melodify"
-                    className="img-fluid"
-                    style={{ maxWidth: "150px" }}
-                  />
-                  <h2 className="card-title mt-3">
-                    <strong>Inicia sesión</strong>
-                  </h2>
-                </div>
-                <LoginForm />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  <>
+    <LoginCard />
+  </>;
 };
 
 export default LoginPage;
