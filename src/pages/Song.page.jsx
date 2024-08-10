6d77781142e25b807044dcf432e3d422927a8ae1
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2024-08-10 15:00:28 -0300

    Updated page structure accordingly.

--- src/pages/Song.page.jsx
+++ src/pages/Song.page.jsx
@@ -1,14 +1,9 @@
-import React from "react";
 import AllSongList from "../components/AllSongList/AllSongList";
 const SongsPage = () => {
   return (
-    <div> 
-      <h1 className="mb-4 mt-2 fw-bolder">
-        Biblioteca
-      </h1>
+    <>
       <AllSongList />
-    </div>
-    
+    </>
   );
 };
 
