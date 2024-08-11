1729c81ef47bf93e1a7d59901cc1c10478746f2f
Carlos <carlosmorales1908@gmail.com>
2024-08-11 01:43:34 -0300

    <Fix in Spinner> Added condition for styles

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,6 +1,6 @@
 const Spinner = ({ layout = null }) => {
   return (
-      <div className="d-flex justify-content-center align-items-center" style={{ minHeight: layout ? "500px" : "auto" }}>
+      <div className="d-flex justify-content-center align-items-center"   style= {layout? {} : { minHeight: "500px" }}>
           <div className="spinner-border color-primary" role="status">
               <span className="visually-hidden"></span>
           </div>
